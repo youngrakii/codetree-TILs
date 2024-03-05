e55abc644a35fcfda4b91f9d7cd09c9d06eda685
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-03-05 12:47:12 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240305/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void Printlines(int n){
+    for(int i=0;i<n;i++){
+        cout<<"12345^&*()_";
+        cout<<"\n";
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    Printlines(n);
+    return 0;
+}
