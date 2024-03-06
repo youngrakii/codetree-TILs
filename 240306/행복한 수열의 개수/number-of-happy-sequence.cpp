2dbac9c6d2f6d20e3b5df9b4eb58f4cab0c76d14
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-03-06 12:58:05 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240306/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -1,38 +1,60 @@
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
 #define MAX_N 100
 
-int n,m;
+int n, m;
 int grid[MAX_N][MAX_N];
 
-int main() {
-    cin>>n>>m;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin>>grid[i][j];
-        }
+int seq[MAX_N];
+
+bool IsHappySequence(){
+    // 주어진 seq가 행복한 수열인지 판단하는 함수입니다.
+    int consecutive_count = 1, max_ccnt = 1;
+    for(int i = 1; i < n; i++) {
+        if (seq[i - 1] == seq[i])
+            consecutive_count++;
+        else
+            consecutive_count = 1;
+        
+        max_ccnt = max(max_ccnt, consecutive_count);
+    }
+    
+    // 최대로 연속한 회수가 m이상이면 true를 반환합니다. 
+    return max_ccnt >= m;
+}
+
+int main(){
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> grid[i][j];
+
+    int num_happy = 0;
+    
+    // 먼저 가로로 행복한 수열의 수를 셉니다.
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++)
+            seq[j] = grid[i][j];
+        
+        if(IsHappySequence())
+            num_happy++;
+    }
+    
+    // 세로로 행복한 수열의 수를 셉니다.
+    for(int j = 0; j < n; j++){
+        // 세로로 숫자들을 모아 새로운 수열을 만듭니다.
+        for(int i = 0; i < n; i++)
+            seq[i] = grid[i][j];
+        
+        if(IsHappySequence())
+            num_happy++;
     }
 
-    int cnt1=0;
-    int cnt2=0;
-    int ans=0;
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(grid[i][j]==grid[i][j+1]){
-                cnt1++;
-                if(cnt1>=m) ans++;
-            }
-            if(grid[i][j]==grid[i+1][j]){
-                cnt2++;
-                if(cnt2>=m) ans++;
-            }
-        }
-    }
-
-    cout<<ans;
+    cout << num_happy;
 
     return 0;
 }
