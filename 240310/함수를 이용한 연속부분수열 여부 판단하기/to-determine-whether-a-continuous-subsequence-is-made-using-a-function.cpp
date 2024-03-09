c65238a27edd21a1a8c4767c12eb953f44431a03
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-03-10 01:02:23 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240310/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 240310/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -1,36 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int n1, n2;
-int A[]={0,};
-int B[]={0,};
-bool IsCoPart(){
-    for(int i=0; i<n1;i++){
-        for(int j=0;j<n2; j++){
-            if(A[i]=B[j]){
-                if(A[i]=B[j+1]){
-                    return true;
-                }
+#define MAX_N 100
 
-            }
+int n1, n2;
+int A[MAX_N], B[MAX_N];
+
+bool IsSame(int n){
+    for(int i=0; i<n2; i++){
+        if(A[i+n]!=B[i]){
+            return false;
+        }
+    }
+    return true;
+}
+bool IsSubsequence(){
+    for(int i=0; i<n1-n2;i++){
+        if(IsSame(i)){
+            return true;
         }
     }
     return false;
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+int main(){
     cin>>n1>>n2;
     for(int i=0; i<n1;i++){
         cin>>A[i];
     }
-    for(int i=0;i<n2;i++){
+    for(int i=0; i<n2; i++){
         cin>>B[i];
     }
-
-    if(IsCoPart()){cout<<"Yes";}
+    if(IsSubsequence()){
+        cout<<"Yes";
+    }
     else{cout<<"No";}
-
     return 0;
 }
