cc96623300a42ebcf2d807a4dddeda3dbaba103a
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-03-10 16:53:59 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240310/그 계절, 그 날/that-season-that-day.cpp
+++ 240310/그 계절, 그 날/that-season-that-day.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 bool IsLeapYear(int y){
-    if(y%4 != 0) return false;
-    if(y%100 != 0) return false;
-    if(y%400==0) return true;
+    if(y%4==0 && y%100 !=0 || y%400==0){
+        return true;
+    }
     return false;
 }
 
