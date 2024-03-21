7130ca35e82ba6e103be432be426c66319bf3d90
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-03-21 12:14:34 +0900

    Title: 변수 출력하기 3, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240321/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a=1;
+    int b=2;
+   const char* c="C";
+    cout<<a<<"->"<<b<<"->"<<c;
+    return 0;
+}
