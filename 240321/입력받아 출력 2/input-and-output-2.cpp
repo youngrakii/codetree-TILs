c8a438c05e19894ba2cca8344d0a652bf69f39c8
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-03-21 15:28:53 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240321/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int num1,num2;
+    cin>>num1;
+    cin.get();
+    cin>>num2;
+    cout<<num1<<num2;
+    return 0;
+}
