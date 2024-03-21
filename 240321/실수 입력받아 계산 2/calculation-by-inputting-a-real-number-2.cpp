b9c9b6c932f57187ffc8fe295382d78a06142c38
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-03-21 14:43:16 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240321/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a;
+    cin>>a;
+    cout<<fixed;
+    cout.precision(2);
+    cout<<a+1.5;
+
+    return 0;
+}
