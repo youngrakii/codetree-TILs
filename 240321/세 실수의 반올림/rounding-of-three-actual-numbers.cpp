2eda4b9eed3a88515dfc656e6e58b87eb82eddc6
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-03-21 14:52:22 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240321/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a,b,c;
+    cin>>a>>b>>c;
+    cout<<fixed;
+    cout.precision(3);
+    cout<<a<<"\n";
+    cout<<b<<"\n";
+    cout<<c<<"\n";
+    return 0;
+}
