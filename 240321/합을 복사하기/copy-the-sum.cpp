4b9f72a070197c73ddfe2ae990177c13c9ff8435
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-03-21 14:29:45 +0900

    Title: 합을 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240321/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a=1;
+    int b=2;
+    int c=3;
+    int sum=a+b+c;
+    a=b=c=sum;
+    cout<<a<<" "<<b<<" "<<c;
+    return 0;
+}
