9690047651a24e32d28477e86d1342148f39677c
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-03-23 17:41:37 +0900

    Title: 특정 위치의 문자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240323/특정 위치의 문자 2/char-in-specific-location-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char arr[10];
+    for(int i=0;i<10;i++){
+        cin>>arr[i];
+    }
+    cout<<arr[1]<<" "<<arr[4]<<" "<<arr[7];
+    return 0;
+}
