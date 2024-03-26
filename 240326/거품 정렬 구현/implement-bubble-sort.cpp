81223ea5ce6ebd829a34f1886b0d7b898048fed6
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-03-26 17:30:54 +0900

    Title: 거품 정렬 구현, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240326/거품 정렬 구현/implement-bubble-sort.cpp
+++ 240326/거품 정렬 구현/implement-bubble-sort.cpp
@@ -1,36 +1,18 @@
-#include <iostream>
+#include <vector>
 #include <algorithm>
-
-#define MAX_N 100
-
+#include <iostream>
 using namespace std;
 
-int n;
-int arr[MAX_N];
-
-void BubbleSort(){
-    bool sorted=true;
-    do{
-        sorted=true;
-        for(int i=0;i<n-1;i++){
-            if(arr[i]>arr[i+1]){
-                int temp=arr[i];
-                arr[i]=arr[i+1];
-                arr[i+1]=temp;
-                sorted=false;
-            }
-        }
-    }while(!sorted);
-}
-
 int main(){
+    int n;
     cin>>n;
+    vector<int> v(n);
     for(int i=0;i<n;i++){
-        cin>>arr[i];
+        cin>>v[i];
     }
-    BubbleSort();
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    sort(v.begin(),v.end());
+    for(auto& i: v){
+        cout<<i<<" ";
     }
     return 0;
 }
