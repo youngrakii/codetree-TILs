9da00cb3df6c32f0b88e86e3f3ea8f79331d027b
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-08-24 04:44:49 +0900

    Title: 1이 k개 이상 존재하는 부분 수열, Time: 201ms, Memory: 4MB, Status: Passed - CodeTree

---
+++ 240824/1이 k개 이상 존재하는 부분 수열/subsequence-with-k-or-more-1's.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> sequence(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> sequence[i];
+    }
+
+    int left = 0;
+    int count_one = 0;
+    int min_length = INT_MAX;
+
+    for (int right = 0; right < n; ++right) {
+        if (sequence[right] == 1) {
+            ++count_one;
+        }
+
+        while (count_one >= k) {
+            min_length = min(min_length, right - left + 1);
+            if (sequence[left] == 1) {
+                --count_one;
+            }
+            ++left;
+        }
+    }
+
+    if (min_length == INT_MAX) {
+        cout << -1 << endl;
+    } else {
+        cout << min_length << endl;
+    }
+
+    return 0;
+}
