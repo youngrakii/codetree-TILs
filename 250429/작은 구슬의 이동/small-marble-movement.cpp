318375750830c6cad633102063c9a93b7191631a
정영락 <117638540+youngrakii@users.noreply.github.com>
2025-04-29 20:36:25 +0900

    Title: 작은 구슬의 이동, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250429/작은 구슬의 이동/small-marble-movement.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+#define ASCII_NUM 128
+#define DIR_NUM 4
+
+using namespace std;
+
+int n;
+int t;
+int x;
+int y;
+int dir;
+int mapper[ASCII_NUM];
+
+int dx[DIR_NUM] = {0, 1, -1,  0};
+int dy[DIR_NUM] = {1, 0,  0, -1};
+
+bool InRange(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
+}
+
+void Simulate() {
+    while(t--) {
+        int nx = x + dx[dir], ny = y + dy[dir];
+        if(InRange(nx, ny)) {
+            x = nx, y = ny;
+        }
+        else
+            dir = 3 - dir;
+    }
+}
+
+int main() {
+    cin >> n >> t;
+    mapper['R'] = 0;
+    mapper['D'] = 1;
+    mapper['U'] = 2;
+    mapper['L'] = 3;
+    
+    char c_dir;
+    cin >> x >> y >> c_dir;
+    x--; y--; dir = mapper[c_dir];
+    
+    Simulate();
+    
+    cout << x + 1 << " " << y + 1<<"\n";
+    return 0;
+}
