3798724dbf117c81b0db125b7209b87813891953
정영락 <117638540+youngrakii@users.noreply.github.com>
2025-04-29 19:54:36 +0900

    Title: 문자에 따른 명령 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250429/문자에 따른 명령 2/text-based-commands2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+#define DIR_NUM 4
+
+using namespace std;
+
+string dirs;
+int x = 0, y = 0;
+int curr_dir = 3;
+
+
+int dx[DIR_NUM] = {1,  0, -1, 0};
+int dy[DIR_NUM] = {0, -1,  0, 1};
+
+int main() {
+    cin >> dirs;
+    
+    for(int i = 0; i < (int) dirs.size(); i++) {
+        char c_dir = dirs[i];
+
+        if(c_dir == 'L')
+            curr_dir = (curr_dir - 1 + 4) % 4;
+
+        else if(c_dir == 'R')
+            curr_dir = (curr_dir + 1) % 4;
+
+        else {
+            x += dx[curr_dir];
+            y += dy[curr_dir];
+        }
+    }
+    
+    cout << x << " " << y;
+    return 0;
+}
